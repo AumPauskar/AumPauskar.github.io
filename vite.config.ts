ca5537beadc353fdf07b4c53dc1b79650a141467
Aum Pauskar <aumpauskar111@gmail.com>
2025-09-07 22:34:34 +0530

    gh pages removed and added workflows to deploy the site

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AumPauskar.github.io/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
